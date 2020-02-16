24e2a4171162d93bed6811acdf466b768a188515
Nguyễn Thanh Liêm <liemnt.uit@gmail.com>
2020-02-16 13:18:24 +0700

    Feat: Headline component with Tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/header";
-import './app.scss';
+import Headline from "./components/headline";
+import "./app.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="main">
+        <Headline header="Posts" desc="Click the button to render Posts!"/>
+      </section>
     </div>
   );
 }
